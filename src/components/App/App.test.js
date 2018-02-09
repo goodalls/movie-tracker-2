8a64129fa7644903761da8ccee2aca199fcbb131
Jeff Goodall <goodallhj@gmail.com>
2018-02-09 08:45:58 -0700

    attempt fail on app.test.js

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 describe('APP', () => {
   it.skip('should match the snapshot', () => {
@@ -10,23 +11,12 @@ describe('APP', () => {
   });
 
   it.skip('should fetchMovies', async () => {
-    const wrapper = shallow(<App />);
-
-    // api.fetchParse = jest.fn()
-    // fetchMovies()
-    // expect(api.fetchParse).toHaveBeenCalled;
     
-    window.fetch = jest.fn().mockImplementation(() =>
-      Promise.resolve({
-        status: 200,
-        json: () =>
-          Promise.resolve({
-            status: 200,
-            movies: []
-          })
-      })
-    );
-   
+    const api = jest.fn();
+    const wrapper = shallow(<App />);
+    const call = fetchMovies();
+    
+    expect(call).toHaveCalled(api.fetchParse);
 
   });
 });
