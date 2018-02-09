9dc6ff7c10df87623cf2ba6c77502b98352a193a
Jeff Goodall <goodallhj@gmail.com>
2018-02-08 21:22:48 -0700

    adding connect to the User.jsx

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,15 +2,15 @@ import { key } from './key';
 
 // const rootUrl = `https://api.themoviedb.org/3/movie/550?api_key=${key}`;
 
-const test = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+export const test = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 
-const fetchParse = async url => {
+export const fetchParse = async url => {
   const response = await fetch(url);
   const parsed = await response.json();
   return parsed;
 };
 
-const movieCleaner = movies => {
+export const movieCleaner = movies => {
   return movies.results.map(movie => {
     return {
       title: movie.title,
@@ -19,7 +19,7 @@ const movieCleaner = movies => {
   });
 };
 
-const logIn = async (user) => {
+export const logIn = async (user) => {
   
   try {
     const response = await fetch('/api/users', {
@@ -42,4 +42,4 @@ const logIn = async (user) => {
 
 };
 
-export default { fetchParse, test, movieCleaner, logIn };
+// export default { fetchParse, test, movieCleaner, logIn };
