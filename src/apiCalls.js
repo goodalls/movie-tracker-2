527f6d9d97c1d0c6bcf0e003632bac464cab2802
Jeff Goodall <goodallhj@gmail.com>
2018-02-09 14:59:02 -0700

    adjusted the api url to show the highest revenue decending

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ import { key } from './key';
 
 // const rootUrl = `https://api.themoviedb.org/3/movie/550?api_key=${key}`;
 
-export const test = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+export const test = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page=1`;
 
 export const fetchParse = async url => {
   const response = await fetch(url);
@@ -19,24 +19,27 @@ export const movieCleaner = movies => {
   });
 };
 
-export const logIn = async user => {
+export const logIn = async (user) => {
+  
   try {
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify(user),
       headers: { 'Content-Type': 'application/json' }
-    });
-    console.log(response);
+      })
+    console.log(response)
     const parsed = await response.json();
 
     if (response.ok) {
-      console.log(parsed);
-      return parsed;
-    }
-  } catch (error) {
-    console.log('error block');
-    return false;
+      console.log(parsed)
+      return parsed
+    } 
+  } catch(error){
+    console.log('error block')
+    return false
   }
+  
+
 };
 
 // export default { fetchParse, test, movieCleaner, logIn };
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
+import apiCalls, { mockApiCalls } from '../../apiCalls.js';
+jest.mock('../../apiCalls.js');
+
 describe('APP', () => {
   it.skip('should match the snapshot', () => {
     const wrapper = shallow(<App />);
