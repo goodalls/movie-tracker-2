33b3563dbc369188c522d7f77fc7a260477fa2ca
Jeff Goodall <goodallhj@gmail.com>
2018-02-10 16:08:54 -0700

    adding actitons test TDD

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -10,13 +10,47 @@ describe('ACTIONS', () => {
     expect(actions.addMovies(mockMovies)).toEqual(expected);
   });
 
-  it('should create an LOG_IN action', () => {
+  it('should create a LOG_IN action', () => {
     const mockUser = {};
     const expected = {
       type: 'LOG_IN',
       user: mockUser
     };
     expect(actions.logIn(mockUser)).toEqual(expected);
+  });
+
+  it('should create a LOG_OUT action', () => {
+    const expected = {
+      type: 'LOG_OUT'
+    };
+    expect(actions.logOut()).toEqual(expected)
+  });
+
+  it('should create an ADD_FAVORITE action', () => {
+    const mockFav = {};
+    const expected = {
+      type: 'ADD_FAVORITE',
+      favorite: mockFav
+    };
+    expect(actions.addFavorite(mockFav)).toEqual(expected);
+  });
+
+  it('should create a REMOVE_FAVORITE action', () => {
+    const mockNotFav = {};
+    const expected = {
+      type: 'REMOVE_FAVORITE',
+      unFavorite: mockNotFav
+    }; 
+    expect(actions.removeFavorite(mockNotFav)).toEqual(expected)
+  });
+
+  it('should create an POPULATE_FAVORITES action', () => {
+    const mockFavs = [];
+    const expected = {
+      type: 'POPULATE_FAVORITES',
+      favorites: mockFavs
+    };
+    expect(actions.populateFavorites(mockFavs)).toEqual(expected);
   });
 });
 
