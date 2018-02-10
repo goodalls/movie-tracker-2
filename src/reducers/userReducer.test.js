d8dd8019a5249cc13e936dca9cb6c4acc2343b6e
Jeff Goodall <goodallhj@gmail.com>
2018-02-10 10:46:45 -0700

    adding first userReducer test passing

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -4,15 +4,15 @@ import * as actions from '../actions/actions';
 describe('userReducer', () => {
   it('userReducer should return state of LOG_IN', () => {
     const expected = { 
-      id: 10
-      name: 'Jeffster'
-      password: 'password'
+      id: 10,
+      name: 'Jeffster',
+      password: 'password',
       email: 'isTheMan@aol.com' 
     };
     const mockUser = { 
-      id: 10
-      name: 'Jeffster'
-      password: 'password'
+      id: 10,
+      name: 'Jeffster',
+      password: 'password',
       email: 'isTheMan@aol.com' 
     };
     expect(user(undefined, actions.logIn(mockUser))).toEqual(expected);
