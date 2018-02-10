05536ff4de21299d3bea83104605b91ff20fd0da
Jeff Goodall <goodallhj@gmail.com>
2018-02-10 09:14:51 -0700

    broken

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,9 @@ import './Card.css';
 const Card = ({ title, poster }) => {
   return (
     <div className="Card">
-      <h2> {title} </h2>
-      <img height='100' width='100' src={`https://image.tmdb.org/t/p/w500/${poster}`} alt={`${title} movie poster`}/>
+      <div className='favorite' >
+      </div>
+      <img height='250' width='200' src={`https://image.tmdb.org/t/p/w500/${poster}`} alt={`${title} movie poster`}/>
     </div>
   );
 };
