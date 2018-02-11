082439598c83c9ec0f73ce565df94896a1ea6a1e
Jeff Goodall <goodallhj@gmail.com>
2018-02-11 15:54:34 -0700

    Added snapshot MovieIndex.test.js

--- src/components/Movies/MovieIndex.test.js
+++ src/components/Movies/MovieIndex.test.js
@@ -3,10 +3,32 @@ import { MovieIndex, mapStateToProps, mapDispatchToProps } from './MovieIndex';
 import { shallow } from 'enzyme';
 
 describe('MOVIE_INDEX', () => {
-  it.skip('should match the snapshot', () => {
-    const wrapper = shallow(<MovieIndex />);
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<MovieIndex movies={[]} user={{}} favorites={[]} />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  // describe('updateFavorites', () => {
+  //   it('should call api once', () => {
+  //     const wrapper = shallow(<MovieIndex movies={[]} user={{}} favorites={[]} />);
+  //     const api = jest.fn();
+  //     window.fetch = jest
+  //       .fn()
+  //       .mockImplementation(() =>
+  //         Promise.resolve({
+  //           json: () =>
+  //             Promise.resolve({
+  //               hello: 'youFool'
+  //             })
+  //         })
+  //       );
+  //     wrapper.instance().updateFavorites();
+  //     wrapper.update();
+  //     expect(api).toHaveBeenCalledTimes(1);
+
+  //   });
+  // });
+  
 
   describe('MDTP and MSTP', () => {
     it('should define movies props for the container MSTP', () => {
@@ -19,21 +41,21 @@ describe('MOVIE_INDEX', () => {
     });
 
     it('should define user props for the container MSTP', () => {
-      const title = 'The Rocky';
-      const poster = '/RockyIsA-BA.jpg';
-      const mockStore = { favorites: [{ title, poster }] };
-      const expected = [{ title, poster }];
-      const mapped = mapStateToProps(mockStore);
-      expect(mapped.favorites).toEqual(expected);
-    });
-
-    it('should define favorites props for the container MSTP', () => {
       const id = 0;
       const name = 'jeffIsGreat';
       const mockStore = { user: [{ id, name }] };
       const expected = [{ id: 0, name: 'jeffIsGreat' }];
       const mapped = mapStateToProps(mockStore);
       expect(mapped.user).toEqual(expected);
+    });
+
+    it('should define favorites props for the container MSTP', () => {
+      const title = 'The Rocky';
+      const poster = '/RockyIsA-BA.jpg';
+      const mockStore = { favorites: [{ title, poster }] };
+      const expected = [{ title, poster }];
+      const mapped = mapStateToProps(mockStore);
+      expect(mapped.favorites).toEqual(expected);
     });
 
     it('should call the dispatch function on MDTP', () => {
