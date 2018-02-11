2f6e8782ea3cb1da52522c08ba2d5c472294f1c4
Jeff Goodall <goodallhj@gmail.com>
2018-02-11 09:30:32 -0700

    set up User.test - mocked out the tests that we will need

--- src/components/User/User.test.js
+++ src/components/User/User.test.js
@@ -3,5 +3,32 @@ import { shallow } from 'enzyme';
 import User from './User';
 
 describe('USER', () => {
-  it('should', () => {});
+  it.skip('should match the snapshot', () => {
+    const wrapper = shallow(<User />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleInput', () => {
+    it('should setState of email and password', () => {
+      
+    
+    });
+    
+  });
+
+  describe('handleSubmit', () => {
+    it('should', async () => {
+      const event = {preventDefault: jest.fn()};
+    
+    });
+  });
+
+  describe('MSTP', () => {
+    
+  });
+
+  describe('MDTP', () => {
+    
+  });
+  
 });
