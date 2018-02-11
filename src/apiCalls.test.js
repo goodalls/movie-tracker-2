7954ff7ddc1f007ef7f9e663cf341a744dfd888f
Nora Gully <nora.gully@gmail.com>
2018-02-11 15:48:27 -0700

    Complete apiCalls.test.js for current functionality

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -61,7 +61,7 @@ describe('apiCalls', () => {
               poster_path: '/4o1yeosjSFMaI9pe1rOkYcZ6hHO.jpg', 
               release_date: 1939, 
               vote_average: 8, 
-              overview: "An overly romanticized version of American Reconstruction", 
+              overview: "An overly romanticized version of American Reconstruction",
               PIZZA: 'TACOS'
             }
           ],
@@ -137,13 +137,12 @@ describe('apiCalls', () => {
     it('makes a fetch request to the database', () => {
       const mockUser = { password: 'tacos', email: 'will@aol.com' }
       const mockUrl = '/api/users'
-      const mockRequestObj =  {
+      const expectedRequestObj = {
           method: 'POST',
           body: JSON.stringify(mockUser),
           headers: { 'Content-Type': 'application/json' }}
-      const expected = JSON.stringify(mockUser)
       api.logIn(mockUser);
-      expect(window.fetch).toHaveBeenCalledWith(mockUrl, mockRequestObj);
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl, expectedRequestObj);
     })
 
     it('it returns a user object with an ID for the store', () => {
@@ -168,32 +167,229 @@ describe('apiCalls', () => {
   });
 
   describe('createUser', () => {
-    // beforeAll(() => {
-    //   window.fetch = jest.fn().mockImplementation(() =>
-    //     Promise.resolve({
-    //       ok: true,
-    //       json: () =>
-    //         Promise.resolve({ data: 
-    //           { id: 0, 
-    //             password: 'tacos', 
-    //             email: 'will@aol.com', 
-    //             name: 'Will' } })
-    //     })
-    //   );
-    // });
-    // it('')
+    beforeAll(() => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: () =>
+            Promise.resolve({ data: { email : "BAchristie@turing.io",
+                                      id : 10, 
+                                      name : "Christie",
+                                      password : "badass"},
+                              message : "Retrieved ONE User",
+                              status :"success" })
+        })
+      );
+    });
+
+    it('should call fetch with the expected parameters', () => {
+      const mockUser = { password: 'badass', email: 'BAchristie@turing.io', name: 'Christie' }
+      const mockUrl = '/api/users/new'
+      const expectedRequestObj =  {
+          method: 'POST',
+          body: JSON.stringify(mockUser),
+          headers: { 'Content-Type': 'application/json' }}
+      api.createUser(mockUser);
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl, expectedRequestObj);
+    })
+
+    it('should return a parsed object with a status of success', async () => {
+      const mockUser = { password: 'badass', email: 'BAchristie@turing.io', name: 'Christie' }
+      const expected = { data: 
+                          { email : "BAchristie@turing.io",
+                            id : 10, 
+                            name : "Christie",
+                            password : "badass" },
+                        message : "Retrieved ONE User",
+                        status :"success" }
+      const user = await api.createUser(mockUser);
+      expect(user).toEqual(expected)
+    })
+
+    it('it returns an error if the request is unsuccessful', () => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.reject({
+          status: 404,
+          json: () => Promise.reject('Error in logIn')
+        })
+      );
+      const mockUser = { password: 'tacos', email: 'will@aol.com' }
+      const expected = 'Error in createUser'
+      const error = api.createUser(mockUser);
+      expect(error).resolves.toEqual(expected)
+    })
   })
 
   describe('addFavorite', () => {
-    
+    beforeAll(() => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: () =>
+            Promise.resolve({status: "success", message: "Movie was added to favorites", id: 10})
+        })
+      );
+    });
+
+    it('calls fetch with the correct parameters', () => {
+      const mockMovie = {
+            movie_id: 36534,
+            title: 'Frozen',
+            poster_path: '/8PADfwpMWK0ASDe4Z5lALCf0zGu.jpg', 
+            release_date: 2015, 
+            vote_average: 8, 
+            overview: "I dunno but the song LET IT GO seems apt today"
+          }
+      const mockUrl = 'api/users/favorites/new'
+      const expectedRequestObj = {
+          method: 'POST',
+          body: JSON.stringify(mockMovie),
+          headers: { 'Content-Type': 'application/json' }}
+      api.addFavorite(mockMovie)
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl, expectedRequestObj);
+    })
+
+    it('returns a nice message if the addition was successful', () => {
+      const mockMovie = {
+            movie_id: 36534,
+            title: 'Frozen',
+            poster_path: '/8PADfwpMWK0ASDe4Z5lALCf0zGu.jpg', 
+            release_date: 2015, 
+            vote_average: 8, 
+            overview: "I dunno but the song LET IT GO seems apt today", 
+            user_id: 10
+          }
+      const expected = {status: "success", message: "Movie was added to favorites", id: 10}
+      const added = api.addFavorite()
+      expect(added).resolves.toEqual(expected)
+    })
+
+    it('it returns an error if the request is unsuccessful', () => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.reject({
+          status: 404,
+          json: () => Promise.reject('Error in addFavorite')
+        })
+      );
+      const mockMovie = {
+            movie_id: 36534,
+            title: 'Frozen',
+            poster_path: '/8PADfwpMWK0ASDe4Z5lALCf0zGu.jpg', 
+            release_date: 2015, 
+            vote_average: 8, 
+            overview: "I dunno but the song LET IT GO seems apt today", 
+            user_id: 30
+          }
+      const expected = 'Error in addFavorite'
+      const error = api.addFavorite(mockMovie);
+      expect(error).resolves.toEqual(expected)
+    })
+
   })
 
   describe('removeFavorite', () => {
-    
+    it('calls fetch with the correct parameters', () => {
+      const mockMovie = {
+            movie_id: 36534,
+            title: 'Frozen',
+            poster_path: '/8PADfwpMWK0ASDe4Z5lALCf0zGu.jpg', 
+            release_date: 2015, 
+            vote_average: 8, 
+            overview: "I dunno but the song LET IT GO seems apt today", 
+            user_id: 30
+          }
+      const expectedUrl = 'api/users/30/favorites/36534'
+      const expectedRequestObj = {
+          method: 'DELETE',
+          body: JSON.stringify({ movie_id: mockMovie.movie_id, user_id: mockMovie.user_id}),
+          headers: { 'Content-Type': 'application/json' }}
+      api.removeFavorite(mockMovie)
+      expect(window.fetch).toHaveBeenCalledWith(expectedUrl, expectedRequestObj);
+    })
+
+    it('returns a nice message if the deletion was successful', () => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: () =>
+            Promise.resolve({status: "success", message: "1 row was deleted."})
+        })
+      );
+      const mockMovie = {
+            movie_id: 36534,
+            title: 'Frozen',
+            poster_path: '/8PADfwpMWK0ASDe4Z5lALCf0zGu.jpg', 
+            release_date: 2015, 
+            vote_average: 8, 
+            overview: "I dunno but the song LET IT GO seems apt today", 
+            user_id: 10
+          }
+      const expected = {status: "success", message: "1 row was deleted."}    
+      const removed = api.removeFavorite(mockMovie)
+      expect(removed).resolves.toEqual(expected)
+    })
+
+    it('it returns an error if the request is unsuccessful', () => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.reject({
+          status: 404,
+          json: () => Promise.reject('Error in removeFavorite')
+        })
+      );
+      const mockMovie = {
+            movie_id: 36534,
+            title: 'Frozen',
+            poster_path: '/8PADfwpMWK0ASDe4Z5lALCf0zGu.jpg', 
+            release_date: 2015, 
+            vote_average: 8, 
+            overview: "I dunno but the song LET IT GO seems apt today", 
+            user_id: 30
+          }
+      const expected = 'Error in removeFavorite'
+      const error = api.removeFavorite(mockMovie);
+      expect(error).resolves.toEqual(expected)
+    })
   })
 
   describe('fetchAllFavorites', () => {
-    
+    beforeAll(() => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          ok: true,
+          json: () =>
+            Promise.resolve({ data: [
+                              { title: 'Bravheart' }, 
+                              { title: 'Trolls'} ] } )
+        })
+      );
+    });
+    it('should call fetch with the correct url', () => {
+      const mockUserId = 3;
+      const expectedUrl = 'api/users/3/favorites'
+      api.fetchAllFavorites(mockUserId);
+      expect(window.fetch).toHaveBeenCalledWith(expectedUrl);
+    })
+
+    it('should return an array of favorited movie objects', () => {
+      const mockUserId = 3;
+      const expected = [ { title: 'Bravheart' }, 
+                         { title: 'Trolls'} ]
+      const movies = api.fetchAllFavorites(mockUserId);
+      expect(movies).resolves.toEqual(expected)
+    })
+
+    it('it returns an error if the request is unsuccessful', () => {
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.reject({
+          status: 404,
+          json: () => Promise.reject('Error in fetchAllFavorites')
+        })
+      );
+      const mockUserId = 3;
+      const expected = 'Error in fetchAllFavorites'
+      const error = api.fetchAllFavorites(mockUserId);
+      expect(error).resolves.toEqual(expected)
+    })
   })
 
 });
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,7 +15,6 @@ export class App extends Component {
     this.state = {};
   }
 
-
   componentDidMount = async () => {
     this.fetchMovies();
   };
