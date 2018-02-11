59c4b9278d0c48d78213d3ad4820144887333078
Jeff Goodall <goodallhj@gmail.com>
2018-02-11 12:06:39 -0700

    adding tests for handleInput function in User.jsx

--- src/components/User/User.test.js
+++ src/components/User/User.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import User from './User';
+import { User } from './User';
 
 describe('USER', () => {
   it.skip('should match the snapshot', () => {
@@ -9,30 +9,57 @@ describe('USER', () => {
   });
 
   describe('handleInput', () => {
-    it('should setState of email and password', () => {
-      
-    
+    it('should setState of email', () => {
+      const wrapper = shallow(<User />);
+      const expected = 'howdy';
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        target: {
+          name: 'email',
+          value: 'howdy'
+        }
+      };
+      wrapper.instance().handleInput(mockEvent);
+      wrapper.update();
+      expect(wrapper.state().email).toEqual(expected);
     });
-    
+
+    it('should setState of password', () => {
+      const wrapper = shallow(<User />);
+      const expected = 'whats up doc';
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        target: { name: 'password', value: 'whats up doc' }
+      };
+      wrapper.instance().handleInput(mockEvent);
+      wrapper.update();
+      expect(wrapper.state().password).toEqual(expected);
+    });
   });
 
   describe('handleSubmit', () => {
-    it('should', async () => {
-      const event = {preventDefault: jest.fn()};
-    
+    it.skip('should', async () => {
+      mockEvent = { preventDefault: jest.fn() };
     });
   });
 
   describe('MSTP', () => {
-    it('should', () => {
-    
+    it.skip('should define user props for the container MSTP', () => {
+      const id = 0;
+      const name = 'jeffIsGreat';
+      const mockStore = { user: { id, name } };
+      const expected = { id: 0, name: 'jeffIsGreat' };
+      const mapped = mapStateToProps(mockStore);
+      expect(mapped.user).toEqual(expected);
     });
   });
 
   describe('MDTP', () => {
-    it('should', () => {
-    
+    it.skip('should call the dispatch function on MDTP', () => {
+      const mockDispatch = jest.fn();
+      const mapped = mapDispatchToProps(mockDispatch);
+      mapped.addMovies();
+      expect(mockDispatch).toHaveBeenCalled();
     });
   });
-  
 });
