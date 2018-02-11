dbc2184e0beced723bb42df9f2eeb8a8902973f9
Jeff Goodall <goodallhj@gmail.com>
2018-02-11 15:17:06 -0700

    adding tests to MovieIndex.test.js MSTP and MDTP tests passing

--- src/components/Movies/MovieIndex.jsx
+++ src/components/Movies/MovieIndex.jsx
@@ -1,49 +1,50 @@
 import React, { Component } from 'react';
-import Card from '../Card/Card';
-import { connect } from 'react-redux';
+import { populateFavorites } from '../../actions/actions';
 import { withRouter } from 'react-router-dom';
 import * as api from '../../apiCalls';
-import { populateFavorites } from '../../actions/actions';
-
-import './MovieIndex.css';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import './MovieIndex.css';
 
 export class MovieIndex extends Component {
-  
   componentDidMount = async () => {
     if (this.props.user.id) {
       this.updateFavorites();
     }
-  }
+  };
 
   updateFavorites = async () => {
     const favorites = await api.fetchAllFavorites(this.props.user.id);
-    this.props.populateFavorites(favorites)
-  }
+    this.props.populateFavorites(favorites);
+  };
 
-  handleClick = async (event) => {
+  handleClick = async event => {
     const { movies, user, favorites } = this.props;
     const user_id = user.id;
     const { id } = event.target;
-    const clicked = movies.find(movie => movie.movie_id === parseInt(id))
-    const movie = Object.assign({}, {...clicked}, {user_id})
+    const clicked = movies.find(movie => movie.movie_id === parseInt(id));
+    const movie = Object.assign({}, { ...clicked }, { user_id });
     if (!favorites.find(movie => movie.movie_id === parseInt(id))) {
-      const response = await api.addFavorite(movie)
-      console.log('response to api.addFav' + response)
+      const response = await api.addFavorite(movie);
+      console.log('response to api.addFav' + response);
     } else {
-      const response = await api.removeFavorite(movie)
-      console.log(response)
-    } 
+      const response = await api.removeFavorite(movie);
+      console.log(response);
+    }
     this.updateFavorites();
-  }
-
+  };
 
   movieCards = () => {
     const { movies } = this.props;
-    return movies.map( movie => {
-      return <Card movie={movie}
-                   handleClick={this.handleClick}
-                   key={movie.movie_id} />;
+    return movies.map(movie => {
+      return (
+        <Card
+          movie={movie}
+          handleClick={this.handleClick}
+          key={movie.movie_id}
+        />
+      );
     });
   };
 
@@ -52,21 +53,23 @@ export class MovieIndex extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  movies: state.movies, 
-  user: state.user, 
+export const mapStateToProps = state => ({
+  movies: state.movies,
+  user: state.user,
   favorites: state.favorites
 });
 
-const mapDispatchToProps = dispatch => ({
+export const mapDispatchToProps = dispatch => ({
   populateFavorites: favorites => dispatch(populateFavorites(favorites))
-})
+});
 
 MovieIndex.propTypes = {
-  movies: PropTypes.array, 
+  movies: PropTypes.array,
   user: PropTypes.object,
-  favorites: PropTypes.array, 
+  favorites: PropTypes.array,
   populateFavorites: PropTypes.func
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MovieIndex));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(MovieIndex)
+);
--- src/components/Movies/MovieIndex.test.js
+++ src/components/Movies/MovieIndex.test.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import MovieIndex from './MovieIndex';
+import { MovieIndex, mapStateToProps, mapDispatchToProps } from './MovieIndex';
 import { shallow } from 'enzyme';
 
 describe('MOVIE_INDEX', () => {
   it.skip('should match the snapshot', () => {
     const wrapper = shallow(<MovieIndex />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('MDTP and MSTP', () => {
+    it('should define movies props for the container MSTP', () => {
+      const title = 'The Rocky';
+      const poster = '/RockyIsA-BA.jpg';
+      const mockStore = { movies: [{ title, poster }] };
+      const expected = [{ title, poster }];
+      const mapped = mapStateToProps(mockStore);
+      expect(mapped.movies).toEqual(expected);
+    });
+
+    it('should define user props for the container MSTP', () => {
+      const title = 'The Rocky';
+      const poster = '/RockyIsA-BA.jpg';
+      const mockStore = { favorites: [{ title, poster }] };
+      const expected = [{ title, poster }];
+      const mapped = mapStateToProps(mockStore);
+      expect(mapped.favorites).toEqual(expected);
+    });
+
+    it('should define favorites props for the container MSTP', () => {
+      const id = 0;
+      const name = 'jeffIsGreat';
+      const mockStore = { user: [{ id, name }] };
+      const expected = [{ id: 0, name: 'jeffIsGreat' }];
+      const mapped = mapStateToProps(mockStore);
+      expect(mapped.user).toEqual(expected);
+    });
+
+    it('should call the dispatch function on MDTP', () => {
+      const mockDispatch = jest.fn();
+      const mapped = mapDispatchToProps(mockDispatch);
+      mapped.populateFavorites();
+      expect(mockDispatch).toHaveBeenCalled();
+    });
   });
 });
